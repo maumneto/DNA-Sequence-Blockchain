c639bd82bd1a4dd122c2b568b49a2949d0221de1
Maurício Moreira Neto <maumneto@gmail.com>
2019-04-29 08:33:33 -0300

    commit

--- LoadDataSet.py
+++ LoadDataSet.py
@@ -1,10 +1,16 @@
 import csv
 
 def datasetOpening():
-    with open(DNAsequenceDATASET.csv) as csv_file:
-        csv_reader = csv.reader(csv_file)
-        cabecalho = False
-        print(csv_reader)
+    with open('DNAsequenceDATASET.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=';')
+        #print(csv_reader)
+        cabecalho = True
+        for row in csv_reader:
+            if cabecalho:
+                print(f'Nomes das colunas: {", ".join(row)}')
+                cabecalho = False
+            else:
+                print(f'{", ".join(row)}')
 
 if __name__ == '__main__':
     print("Olá")
