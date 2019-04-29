d37913649fca80e1015c67d03cdb374d81a14a07
Maurício Moreira Neto <maumneto@gmail.com>
2019-04-29 07:50:38 -0300

    commit
    colocando o formatador correto

--- LoadDataSet.py
+++ LoadDataSet.py
@@ -1,1 +1,11 @@
 import csv
+
+def datasetOpening():
+    with open(DNAsequenceDATASET.csv) as csv_file:
+        csv_reader = csv.reader(csv_file)
+        cabecalho = False
+        print(csv_reader)
+
+if __name__ == '__main__':
+    print("Olá")
+    datasetOpening()
